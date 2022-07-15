cf5f3bab5b9d8654b8ffd8a3d744885d10e272dd
Connor <Woodconnor13@gmail.com>
2022-07-15 15:15:49 -0400

    Joining data exercises 1 and 2

---
+++ JoiningData/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoiningData
+{
+    internal class Customer
+    {
+        //properties
+        public string CustomerName { get; set; }
+        public string Item { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        //constrcutor
+        public Customer(string _customerName, string _item, decimal _price, int _quantity)
+        {
+            CustomerName = _customerName;
+            Item = _item;
+            Price = _price;
+            Quantity = _quantity;
+        }
+    }
+}
---
+++ JoiningData/Program.cs
@@ -0,0 +1,69 @@
+﻿using JoiningData;
+
+List<Customer> Customers = new List<Customer>()
+{
+    new Customer("Acme Hardware", "Mouse", 25m, 3),
+    new Customer("Acme Hardware", "Keyboard", 45m, 2),
+    new Customer("Falls Realty", "Macbook", 800m, 2),
+    new Customer("Julie's Morning Diner", "iPad", 525m, 1),
+    new Customer("Julie's Morning Diner", "Credit Card Reader", 45, 1)
+};
+
+List<Customer> distinctCustomerOrders = Customers.GroupBy(o => o.CustomerName).Select(c => c.First()).ToList();
+
+//exercise 1
+//foreach (Customer distinctCustomer in distinctCustomerOrders)
+//{
+//    Console.WriteLine(distinctCustomer.CustomerName);
+//    List<Customer> CustomerOrders = Customers.Where(o => o.CustomerName == distinctCustomer.CustomerName).ToList();
+//    Console.WriteLine("Item\tPrice\tQuantity\tTotal");
+//    foreach (Customer o in CustomerOrders)
+//    {
+//        Console.WriteLine($"{o.Item}\t{o.Price}\t{o.Quantity}\t{o.Price * o.Quantity}");
+//    }
+//    Console.WriteLine();
+//}
+
+//exercise 2
+//foreach (Customer distinctCustomer in distinctCustomerOrders)
+//{
+//    decimal total = 0;
+//    Console.WriteLine(distinctCustomer.CustomerName);
+//    List<Customer> CustomerOrders = Customers.Where(o => o.CustomerName == distinctCustomer.CustomerName).ToList();
+//    Console.WriteLine("Customer\tItem\tPrice\tQuantity\tTotal");
+//    foreach (Customer o in CustomerOrders)
+//    {
+//        Console.WriteLine($"{o.Item}\t{o.Price}\t{o.Quantity}\t{o.Price * o.Quantity}");
+//        total += o.Price * o.Quantity;
+//    }
+//    Console.WriteLine(total);
+//    Console.WriteLine();
+//}
+
+//exercise 3
+foreach (Customer distinctCustomer in distinctCustomerOrders)
+{
+    decimal total = 0;
+    Console.WriteLine(distinctCustomer.CustomerName);
+    List<Customer> CustomerOrders = Customers.Where(o => o.CustomerName == distinctCustomer.CustomerName).ToList();
+    Console.WriteLine("Customer\tItem\tPrice\tQuantity\tTotal");
+    foreach (Customer o in CustomerOrders)
+    {
+        Console.WriteLine($"{o.Item}\t{o.Price}\t{o.Quantity}\t{o.Price * o.Quantity}");
+        total += o.Price * o.Quantity;
+    }
+    Console.WriteLine(total);
+    Console.WriteLine();
+}
+
+//foreach (Customer c in Customers)
+//{
+//    if (UniqueCustomers.Contains(c.CustomerName))
+//    {
+//        continue;
+//    }
+//    else
+//    {
+//        UniqueCustomers.Add(c.CustomerName);
+//    }
+//}
